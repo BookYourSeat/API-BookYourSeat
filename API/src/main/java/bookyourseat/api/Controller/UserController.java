1b0a90e1f94d2e714922068b2cb93414271b4480
sergio.kappes <sergiokappes@gmail.com>
2022-10-20 21:59:13 -0300

    Controller dependencies

--- API/src/main/java/bookyourseat/api/Controller/UserController.java
+++ API/src/main/java/bookyourseat/api/Controller/UserController.java
@@ -1,12 +1,24 @@
 package bookyourseat.api.Controller;
 
+import User.Model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 public class UserController {
-    @GetMapping("User/Get")
-    public String GetUser() {
-        return String.format("Logging in!");
+    @GetMapping("User/GetById")
+    public User GetUser(UUID Id) {
+        User newUser = new User();
+        return newUser;
+    }
+
+    @GetMapping("User/GetAll")
+    public List<User> GetAllUsers() {
+        ArrayList users = new ArrayList<User>();
+        return users;
     }
 }
